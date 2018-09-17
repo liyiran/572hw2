a09e1a76827733ad34ecf03d0f4e28854c69f552
liyiran <liy@worldticket.net>
2018-09-16 20:57:53 -0700

    initial commit

---
+++ src/test/java/AssignmentTest.java
@@ -0,0 +1,46 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2003, 2004 WorldTicket A/S
+ * All rights reserved.
+ */
+
+/**
+ * @author Yiran Li / 2M business applications a|s
+ * @version $Revision$ $Date$
+ */
+
+public class AssignmentTest {
+//    @Test
+//    @Disabled
+//    public void test1() throws Exception {
+//        String crawlStorageFolder = "result";
+//        int numberOfCrawlers = 7;
+//
+//        CrawlConfig config = new CrawlConfig();
+//        config.setCrawlStorageFolder(crawlStorageFolder);
+//
+//        /*
+//         * Instantiate the controller for this crawl.
+//         */
+//        PageFetcher pageFetcher = new PageFetcher(config);
+//        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+//        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+//        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+//
+//        /*
+//         * For each crawl, you need to add some seed urls. These are the first
+//         * URLs that are fetched and then the crawler starts following links
+//         * which are found in these pages
+//         */
+//        controller.addSeed("http://www.ics.uci.edu/~lopes/");
+//        controller.addSeed("http://www.ics.uci.edu/~welling/");
+//        controller.addSeed("http://www.ics.uci.edu/");
+//
+//        /*
+//         * Start the crawl. This is a blocking operation, meaning that your code
+//         * will reach the line after this only when crawling is finished.
+//         */
+//        controller.start(MyCrawler.class, numberOfCrawlers);
+//    }
+}
